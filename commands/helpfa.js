66042c04ed4240f934e4d9dff5aa86d537031cf6
amirzarei007 <amirshayan1381@yahoo.com>
2021-02-13 21:57:17 +0330

    #35

--- commands/helpfa.js
+++ commands/helpfa.js
@@ -18,7 +18,7 @@ var pagination = require('discord.js-pagination');
         const help = new Discord.MessageEmbed()
         .setTitle('برای اد کرد ربات در سرور خود اینجا کلیک کنید ')
         .setURL('https://discord.com/api/oauth2/authorize?client_id=726137789757915177&permissions=8&scope=bot')
-        
+        .setImage('https://cdn.discordapp.com/attachments/794975466523328559/810215124110934076/sylvanas.gif')
         .setTimestamp()
        
         
